3ce162c9c9cba4e816990aaaae2abc51834eac29
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-07 14:52:19 -0300

    add TestaVariaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("ola novo teste");
+
+        int  idade;
+        idade = 31;
+        idade = 30 + 10;
+  
+        System.out.println("A idade é " + idade);
+
+    }
+
+}
