1ea26d6575b8e56adaa9f1375f800dd5979e72dd
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-10 12:36:50 -0300

    add aula caracteres

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,13 @@
+public class TestaCaracteres {
+    public static void main(String[] args) {
+        char letra = 'a' ;
+        char valor = 66;
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+
+        String palavra = "alura cursos online de tecnologia";
+        System.out.println(palavra);
+    }
+}
