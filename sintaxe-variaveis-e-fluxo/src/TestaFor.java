e20f4b7b9ec666898ea867ecb441891613c39480
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-11 17:35:08 -0300

    TestaFor

---
+++ sintaxe-variaveis-e-fluxo/src/TestaFor.java
@@ -0,0 +1,11 @@
+public class TestaFor {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 10; count++) {
+            System.out.println(count);
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaSomatoria.java
@@ -0,0 +1,12 @@
+public class TestaSomatoria {
+    public static void main(String[] args) {
+        int count = 0;
+        int total = 0;
+
+        while (count <= 10) {
+            total += count;
+            count++;
+        }
+        System.out.println("O total é: "+total);
+    }
+}
