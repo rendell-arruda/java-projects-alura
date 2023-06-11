7ea121f3942301cf398c0ec1b8e9ef7e78ef9193
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-11 17:55:30 -0300

    for com divisiveis por 3

---
+++ sintaxe-variaveis-e-fluxo/src/MultiTres.java
@@ -0,0 +1,11 @@
+public class MultiTres {
+    public static void main(String[] args) {
+        int numero = 100;
+        for (int i = 0; i < numero; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
