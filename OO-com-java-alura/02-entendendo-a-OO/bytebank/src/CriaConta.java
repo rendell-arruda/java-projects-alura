0d7e92a9be60dbe3639ce684d6ffb8f3511803b4
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-12 21:53:38 -0300

    instanciar a segunda conta

--- OO-com-java-alura/02-entendendo-a-OO/bytebank/src/CriaConta.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank/src/CriaConta.java
@@ -1,7 +1,15 @@
 public class CriaConta {
     public static void main(String[] args) {
+        //primeira conta
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200.0;
+        primeiraConta.saldo += 100;
         System.out.println(primeiraConta.saldo);
+
+        //segunda conta
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50.0;
+        System.out.println(segundaConta.saldo);
+
     }
 }
