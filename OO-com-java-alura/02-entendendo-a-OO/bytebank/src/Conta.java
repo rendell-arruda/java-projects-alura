330bab56ae0ae609ec844aa77b2078fdfb6ca0e9
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-14 20:58:57 -0300

    declaracao de metodos

--- OO-com-java-alura/02-entendendo-a-OO/bytebank/src/Conta.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank/src/Conta.java
@@ -5,8 +5,30 @@ public class Conta {
     int numero;
     String titular;
 
-    public void deposita(double valor){
-        this.saldo = this.saldo + valor;
+    public void deposita(double valor) {
+        this.saldo += valor;
+
+    }
+
+    public boolean saca(double valor) {
+
+        if (this.saldo >= valor) {
+//          this.saldo -= valor;
+            deposita(valor);
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
 
     }
 }
--- OO-com-java-alura/02-entendendo-a-OO/bytebank/src/TestaMetodo.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank/src/TestaMetodo.java
@@ -6,5 +6,24 @@ public class TestaMetodo {
 
         System.out.println(contaDoPaulo.saldo);
 
+        boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuRetirar);
+
+        //conta da marcela
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+
+
+        boolean sucessoTranferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+        if (sucessoTranferencia) {
+            System.out.println("Transferência feita com sucesso");
+        } else {
+            System.out.println("You have no money!");
+
+        }
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo);
+
     }
 }
