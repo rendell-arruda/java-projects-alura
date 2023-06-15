878007c6fa4342a6fcc57fb049a468933561ea21
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-15 10:08:34 -0300

    getters e setters de referencia

--- OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/Conta.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/Conta.java
@@ -51,4 +51,12 @@ public class Conta {
     public void setAgencia(int agencia) {
         this.agencia = agencia;
     }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
 }
--- OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/TestaGetESet.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/TestaGetESet.java
@@ -2,7 +2,20 @@ public class TestaGetESet {
     public static void main(String[] args) {
         Conta conta = new Conta();
         conta.setNumero(1337);
-        System.out.println(conta.getNumero());
+//      System.out.println(conta.getNumero());
+
+//      cria um cliente
+        Cliente paulo = new Cliente();
+
+//      conta.titular = paulo;
+        paulo.setNome("Paulo Silveira");
+
+//      vincula titular ao cliente paulo
+        conta.setTitular(paulo);
+        System.out.println(conta.getTitular().getNome());
+
+        conta.getTitular().setProfissao("Programador");
+        System.out.println(conta.getTitular().getProfissao());
 
     }
 }
