23f175479c349b13c5021270d89681753e1b71c5
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-15 09:51:23 -0300

    getters e setters

--- OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/Conta.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank-encapsulado/src/Conta.java
@@ -1,9 +1,9 @@
 public class Conta {
     //atributos de conta
-   private double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
 
     public void deposita(double valor) {
         this.saldo += valor;
@@ -36,4 +36,19 @@ public class Conta {
         return this.saldo;
     }
 
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 }
