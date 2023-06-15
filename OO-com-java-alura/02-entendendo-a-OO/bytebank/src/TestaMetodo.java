b86fe3fcb9a42bf9772963913584d920cae945b0
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-15 07:57:47 -0300

    add bytebank composto

--- OO-com-java-alura/02-entendendo-a-OO/bytebank/src/TestaMetodo.java
+++ OO-com-java-alura/02-entendendo-a-OO/bytebank/src/TestaMetodo.java
@@ -25,5 +25,8 @@ public class TestaMetodo {
         System.out.println(contaDaMarcela.saldo);
         System.out.println(contaDoPaulo.saldo);
 
+        contaDoPaulo.titular = "paulo silveira";
+        System.out.println(contaDoPaulo.titular);
+
     }
 }
