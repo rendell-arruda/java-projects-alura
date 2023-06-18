5e2a8b966aad3b3f71d76755ccd416da9e186a06
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-18 15:48:29 -0300

    comecando heranca com gerente

---
+++ OO-com-java-alura/03-herenca/bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,18 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente gerente1 = new Gerente();
+        gerente1.setNome("Marco");
+        gerente1.setCpf("11111111111");
+        gerente1.setSalario(5000.00);
+
+        System.out.println(gerente1.getNome());
+        System.out.println(gerente1.getCpf());
+        System.out.println(gerente1.getSalario());
+
+        gerente1.setSenha(2222);
+        boolean autenticou = gerente1.autentica(2222);
+        System.out.println(autenticou);
+
+
+    }
+}
