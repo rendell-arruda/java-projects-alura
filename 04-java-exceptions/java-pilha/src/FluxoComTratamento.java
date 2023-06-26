527e32258cbf42851b34644d6eb4a7909be4606b
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-26 19:34:09 -0300

    criando uma excecao

--- 04-java-exceptions/java-pilha/src/FluxoComTratamento.java
+++ 04-java-exceptions/java-pilha/src/FluxoComTratamento.java
@@ -1,11 +1,11 @@
-public class Fluxo {
+public class FluxoComTratamento {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
             metodo1();
         } catch (ArithmeticException | NullPointerException ex) {
             String msg = ex.getMessage();
-            System.out.println("Exception: " + msg);
+            System.out.println("Msg: " + msg);
             ex.printStackTrace();
         }
         System.out.println("Fim do main");
@@ -20,9 +20,12 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-
-        throw new ArithmeticException("Deu errado");
-
-//        System.out.println("Fim do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            int a = i / 0;
+//            Conta c = null;
+//            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
     }
 }
