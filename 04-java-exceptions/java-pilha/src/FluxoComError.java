b5331817490a70fdcef38e63a2262d41c727738e
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-27 11:32:13 -0300

    error, checked e unchecked

--- 04-java-exceptions/java-pilha/src/FluxoComError.java
+++ 04-java-exceptions/java-pilha/src/FluxoComError.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComError {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
@@ -11,18 +11,17 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
 
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
-        System.out.println("Ini do metodo2");
+    private static void metodo2() {
 
-        throw new MinhaExcecao("Deu muito errado");
-
-//        System.out.println("Fim do metodo2");
+        System.out.println("Ini metodo 2");
+        metodo2();
+        System.out.println("fim metodo 2");
     }
 }
