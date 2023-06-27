6a15321f17d5f524a7203abe53dd595d94d4c924
Rendell Arruda <rendellarruda3@gmail.com>
2023-06-27 18:26:50 -0300

    capturar qualquer excecao

---
+++ 04-java-exceptions/java-pilha/src/TestaContaComExcessaoChecked.java
@@ -0,0 +1,10 @@
+public class TestaContaComExcessaoChecked {
+    public static void main(String[] args) {
+        Conta c = new Conta();
+       try{
+           c.deposita();
+       }catch (MinhaExcecao ex){
+           System.out.println("Tratamento da excecao...");
+       }
+    }
+}
