5bceef4a17b596e23d69078cba584307fc6cc374
Rendell Arruda <rendellarruda3@gmail.com>
2023-07-01 14:52:12 -0300

    arrays do tipo object

--- java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
+++ java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
@@ -1,6 +1,9 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TestArrayReferencias {
 
@@ -8,19 +11,26 @@ public class TestArrayReferencias {
 
 		// int[] idades = new int[5];
 
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0] = cc1;
+		referencias[0] = cc1;
+		
 
-		ContaCorrente cc2 = new ContaCorrente(22, 33);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(22, 33);
+		referencias[1] = cc2;
+		
+		
+		 Cliente cliente = new Cliente();
+		 referencias[2] = cliente;
 		
 //		System.out.println(cc2.getNumero());
-		
-		System.out.println(contas[1].getNumero());
-		
-		ContaCorrente ref = contas[1];
+//		 Object referenciaGenerica = contas[1];
+//		
+//		System.out.println(referenciaGenerica.getNumero());
+//		
+		 
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
 		System.out.println(ref.getNumero());
 		System.err.println(cc2.getNumero());
 	}
