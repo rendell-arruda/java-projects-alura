9eaf21bd379b4b6b406f3587c92ed8f3e41831c2
Rendell Arruda <rendellarruda3@gmail.com>
2023-07-01 10:54:45 -0300

    inicio de arrays

---
+++ java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -0,0 +1,36 @@
+package br.com.bytebank.banco.test;
+
+import java.util.Iterator;
+
+public class Teste {
+
+//	Array []
+
+	public static void main(String[] args) {
+
+		int[] idades = new int[5];// inicializa o Array com zero
+
+//		idades[0] = 29;
+//		idades[1] = 19;
+//		idades[2] = 23;
+//		idades[3] = 22;
+//		idades[4] = 49;
+//		int idade4 = idades[2];
+//		
+//		System.out.println(idade4);
+//		
+//		System.out.println(idades.length);
+
+		for (int i = 0; i < idades.length; i++) {
+
+			idades[i] = i * i;
+			System.out.println(i);
+		}
+
+		for (int i = 0; i < idades.length; i++) {
+
+			System.out.println(idades[i]);
+		}
+	}
+
+}
